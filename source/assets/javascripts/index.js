1b4184ade7bf83b42f69d24886584005f1c06387
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 17:03:21 +0900

    onmouseover event listener

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,10 +1,8 @@
 import 'bootstrap';
 
-let numberOfIcons = document.querySelectorAll('.navigation-button').length;
-
+const numberOfIcons = document.querySelectorAll('.navigation-button').length;
 
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.navigation-button')[i].addEventListener("click", function () {
-    alert('I got clicked')
-  })
-};
+  document.querySelectorAll('.navigation-button')[i].addEventListener('onmouseover', function(event) {
+    event.style.width = '100px';});
+}
