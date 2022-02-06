3e802cb997c745bc4b9c48878c51fdadb7303ae8
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 17:15:21 +0900

    adding this

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 const numberOfIcons = document.querySelectorAll('.navigation-button').length;
 
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.navigation-button')[i].addEventListener('onmouseover', function(event) {
-    event.style.width = '100px';});
+  document.querySelectorAll('.navigation-button')[i].addEventListener('onmouseover', function () {
+    this.style.width = '100px';
+  });
 }
