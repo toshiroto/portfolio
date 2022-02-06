2ca69790dd4b4839c666a8cd5cbd4f7e97e05fd7
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 17:59:34 +0900

    mouseenter

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 const numberOfIcons = document.querySelectorAll('.devicon').length;
 
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.devicon')[i].addEventListener('mouseon', function () {
+  document.querySelectorAll('.devicon')[i].addEventListener('mouseenter', function () {
     if this.style.width = '100px';
   });
 }
