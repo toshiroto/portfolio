dad0ddf247c1832de5b4bb653fce0d3055aba303
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 17:24:40 +0900

    what about mouseover

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 const numberOfIcons = document.querySelectorAll('.img').length;
 
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.img')[i].addEventListener('onmouseover', function () {
+  document.querySelectorAll('.img')[i].addEventListener('mouseover', function () {
     this.style.width = '100px';
   });
 }
