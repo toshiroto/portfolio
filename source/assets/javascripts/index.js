8a02ec4a0073aacff055c2b670e426e44ffdbdeb
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 17:40:26 +0900

    how about mousemove

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 const numberOfIcons = document.querySelectorAll('.devicon').length;
 
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.devicon')[i].addEventListener('mouseover', function () {
+  document.querySelectorAll('.devicon')[i].addEventListener('mousemove', function () {
     this.style.width = '100px';
   });
 }
