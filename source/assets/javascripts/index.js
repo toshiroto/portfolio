ad4ba529ce26fead9ed2a0ca38115cba21805739
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 16:44:03 +0900

    } added

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -6,4 +6,5 @@ let numberOfIcons = document.querySelectorAll('.navigation-button').length;
 for (let i = 0; i < numberOfIcons; i++) {
   document.querySelectorAll('.navigation-button')[i].addEventListener("click", function () {
     alert('I got clicked')
-});
+  })
+};
