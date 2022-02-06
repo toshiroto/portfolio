1a7e4abe0a7e96fdfdcbb66e0771b45ec77f2beb
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 18:08:34 +0900

    add if statment

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,7 +3,11 @@ import 'bootstrap';
 const numberOfIcons = document.querySelectorAll('.devicon').length;
 
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.devicon')[i].addEventListener('mouseenter', function () {
-    if this.style.width = '100px';
+  document.querySelectorAll('.devicon')[i].addEventListener('mouseenter', function() {
+    if () {
+      this.style.width = '100px';
+    } else {
+      this.style.width = '80px';
+    }
   });
 }
