70f4113337bba9cd1f13ea18b7f07d981e08a2fd
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-06 16:41:28 +0900

    navigation button JS

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,2 +1,9 @@
 import 'bootstrap';
 
+let numberOfIcons = document.querySelectorAll('.navigation-button').length;
+
+
+for (let i = 0; i < numberOfIcons; i++) {
+  document.querySelectorAll('.navigation-button')[i].addEventListener("click", function () {
+    alert('I got clicked')
+});
