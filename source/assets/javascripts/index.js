a0853e35ffd60266d3d6f8f975174f94f8992ead
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-11 09:49:25 +0900

    transition effect added

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -2,10 +2,13 @@ import 'bootstrap';
 
 const numberOfIcons = document.querySelectorAll('.devicon').length;
 
-
 for (let i = 0; i < numberOfIcons; i++) {
-  document.querySelectorAll('.devicon')[i].addEventListener('mouseenter', function () {
+  const magnifyIcon = document.querySelectorAll('.devicon')[i];
+  magnifyIcon.addEventListener('mouseenter', function () {
     this.style.width = '100px';
+  });
+  magnifyIcon.addEventListener('transitionend', function () {
+    this.style.width = '80px';
   });
 
   document.querySelectorAll('.devicon')[i].addEventListener('mouseleave', function () {
